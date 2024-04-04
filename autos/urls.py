98c0b5a95513b88e292ee716c9079ea81a273311
Julian <julian.nicholas.weidner@gmail.com>
2024-04-04 14:31:17 +0200

    Updated url '' to be the login page

--- autos/urls.py
+++ autos/urls.py
@@ -5,17 +5,17 @@ from . import views
 app_name = 'autos'
 
 urlpatterns = [
-  
-    path('', views.AutosListView.as_view(), name = "autos_list_view"), #list
-    path('create/', views.AutosCreateView.as_view(), name = 'autos_create_view'), #Create
-    path('<int:pk>/', views.AutosDetailView.as_view(), name = 'autos_detail_view'), #Read, 
-    path('<int:pk>/update/', views.AutosUpdateView.as_view(), name = 'autos_update_view'), #update
-    path('<int:pk>/delete/', views.AutosDeleteView.as_view(), name = 'autos_delete_view'), #delete
+    path('', views.MainView.as_view(), name='main'),
+    path('autos/', views.AutosListView.as_view(), name = "autos_list_view"), #list
+    path('autos/create/', views.AutosCreateView.as_view(), name = 'autos_create_view'), #Create
+    path('autos/<int:pk>/', views.AutosDetailView.as_view(), name = 'autos_detail_view'), #Read, 
+    path('autos/<int:pk>/update/', views.AutosUpdateView.as_view(), name = 'autos_update_view'), #update
+    path('autos/<int:pk>/delete/', views.AutosDeleteView.as_view(), name = 'autos_delete_view'), #delete
 
-    path('makes/', views.MakeListView.as_view(), name = "makes_list_view"), #list
-    path('makes/create/', views.MakeCreateView.as_view(), name='makes_create_view'), 
-    #path('makes/<int:pk>/'), 
-    #path('makes/<int:pk>/update/',),
-    path('makes/<int:pk>/delete/', views.MakeDeleteView.as_view(), name='makes_delete_view'),
+    path('autos/makes/', views.MakeListView.as_view(), name = "makes_list_view"), #list
+    path('autos/makes/create/', views.MakeCreateView.as_view(), name='makes_create_view'), 
+    #path('autos/makes/<int:pk>/'), 
+    #path('autos/makes/<int:pk>/update/',),
+    path('autos/makes/<int:pk>/delete/', views.MakeDeleteView.as_view(), name='makes_delete_view'),
 
 ]
