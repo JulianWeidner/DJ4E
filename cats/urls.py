48835e1cb3cfaa8a8c2bb63e255b74ea3d206da5
Julian <julian.nicholas.weidner@gmail.com>
2024-04-09 19:35:04 +0200

    Idk what happened

--- cats/urls.py
+++ cats/urls.py
@@ -4,5 +4,15 @@ from . import views
 app_name = 'cats'
 
 urlpatterns = [
-    path("", views.MainView.as_view(), name='main')
+    path("", views.MainView.as_view(), name='main'),
+    path("create/", views.CreateCatView.as_view(), name='cat_create'),
+    path("<int:pk>/details/", views.DetailsCatView.as_view(), name='cat_details'),
+    path("<int:pk>/update/", views.UpdateCatView.as_view(), name='cat_update'),
+    path("<int:pk>/delete/", views.DeleteCatView.as_view(), name ='cat_delete'),
+
+
+    path("breed/create/", views.CreateBreedView.as_view(), name='breed_create'),
+    path("breed/", views.ListBreedView.as_view(), name='breed_list'),
+    path("breed/<int:pk>/update/", views.UpdateBreedView.as_view(), name='breed_update'),
+    path("breed/<int:pk>/delete", views.DeleteBreedView.as_view(), name='breed_delete')
 ]
--- cats/views.py
+++ cats/views.py
@@ -1,10 +1,68 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, UpdateView, DetailView, DeleteView, CreateView
+from .models import Cat, Breed
+from .forms import CatForm, BreedForm
 
 # Create your views here.
-class MainView(LoginRequiredMixin, View):
+class MainView(LoginRequiredMixin, ListView):
     template_name = 'cats/main.html'
+    model = Cat
+
+class CreateCatView(LoginRequiredMixin, CreateView):
+    model = Cat
+    form_class = CatForm
+    template_name = 'cats/forms/cat_form.html'
     
-    def get(self, request):
-        return render(request, self.template_name)
+
+class DetailsCatView(LoginRequiredMixin, DetailView):
+    model = Cat
+    template_name = 'cats/cat_details.html'
+
+class UpdateCatView(LoginRequiredMixin, UpdateView):
+    model = Cat
+    form_class = CatForm
+    template_name = 'cats/forms/cat_form.html'
+    success_url = reverse_lazy('cats:main')
+
+
+class DeleteCatView(LoginRequiredMixin, DeleteView):
+    model = Cat
+    template_name = 'cats/forms/delete_form.html'
+    success_url = reverse_lazy('cats:main')
+
+
+
+
+class ListBreedView(LoginRequiredMixin, ListView):
+    model = Breed
+
+
+
+class CreateBreedView(LoginRequiredMixin, CreateView):
+    model = Breed
+    form_class = BreedForm
+    template_name = 'cats/forms/breed_form.html'
+    success_url = reverse_lazy('cats:main')
+
+class DetailsBreedView(LoginRequiredMixin, DetailView):
+    model = Breed
+
+class UpdateBreedView(LoginRequiredMixin, UpdateView):
+    model = Breed
+    form_class = BreedForm
+    template_name = 'cats/forms/breed_form.html'
+    success_url = reverse_lazy('cats:main')
+
+class DeleteBreedView(LoginRequiredMixin, DeleteView):
+    model = Cat
+    template_name = 'cats/forms/delete_form.html'
+    success_url = reverse_lazy('cats:main')
+
+    
+
+
+    
+
