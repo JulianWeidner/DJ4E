d17c5c71449e82d08e91f718bce7654c8879cab2
Julian <julian.nicholas.weidner@gmail.com>
2024-04-17 11:32:44 +0200

    Added price to the update form

--- ads/views.py
+++ ads/views.py
@@ -13,11 +13,11 @@ class AdDetailView(OwnerDetailView):
 class AdCreateView(OwnerCreateView):
     model = Ad
     # List the fields to copy from the Ad model to the Ad form
-    fields = ['title', 'text']
+    fields = ['title', 'text', 'price']
 
 class AdUpdateView(OwnerUpdateView):
     model = Ad
-    fields = ['title', 'text']
+    fields = ['title', 'text', 'price']
     # This would make more sense
     # fields_exclude = ['owner', 'created_at', 'updated_at']
 
