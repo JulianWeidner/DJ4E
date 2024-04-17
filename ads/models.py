5d86e09461076eabb81b3550f70394cc67b1d6a4
Julian <julian.nicholas.weidner@gmail.com>
2024-04-17 10:34:43 +0200

    Added Login Url to basetemplate navbar

--- ads/models.py
+++ ads/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.conf import settings
 
-class Ad(models.Model) :
+class Ad(models.Model):
     title = models.CharField(
             max_length=200,
             validators=[MinLengthValidator(2, "Title must be greater than 2 characters")]
